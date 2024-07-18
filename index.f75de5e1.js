64f3c5a8b607e39f2ba8596ba84138e34f5a7a89
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-18 06:35:21 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const topOffset = wall.clientHeight / 2 - spider.clientHeight / 2;
+const leftOffset = wall.clientWidth / 2 - spider.clientWidth / 2;
+spider.style.top = topOffset + "px";
+spider.style.left = leftOffset + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
